c463108d1f2663100021f08b123df251903a0c3c
Hakan Kocapınar <hknkcpnr01@gmail.com>
2026-03-14 11:33:51 +0300

    chore: update base URL for sitemap and robots.txt

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://mitra-event-web.vercel.app';
+  const baseUrl = 'https://mitraevent.com.tr';
 
   return [
     {
